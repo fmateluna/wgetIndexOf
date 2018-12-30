ceafbe46a5b4f2346b58ad559d1fba75d6377126
Fernando <fernando.mateluna@gmail.com>
2018-12-30 01:35:43 -0300

    Fix

--- wgetindexof.py
+++ wgetindexof.py
@@ -15,23 +15,43 @@ __maintainer__ = 'Fernando Mateluna'
 __status__ = 'CTO'
 
 url = ""
+ext = ""
+text = ""
 thread = 0
 
 
 #obtener todos los valores href o src desde el html
 def get_Href_From_Url(url):
+    print("wget {}".format(url))
     download = []
     ht= urllib.urlopen(url)
     html_page = ht.read()
-    b_object = BeautifulSoup(html_page, "lxml")
+    b_object = BeautifulSoup(html_page, "html5lib")
+    to_download = False
     for link in b_object.find_all('a'):
         url_href = "{}/{}".format(url,link.get('href'))
+        print("get info from : {}".format(url_href))
         try:
-            ret = urllib2.urlopen(url_href)
-            if ret.code == 200:
-                print("{} {}".format(ret.code, url_href))
-                os.system("wget {}".format(url_href))
-                download.append(url_href)
+            to_download = False
+            if (ext == "") or (text == ""):
+                to_download = True
+            else:
+                if url_href.endswith(ext):
+                    to_download = True
+                print("get {}".format(url_href.split("/")[-1]))
+                if text in url_href.split("/")[-1]:
+                    to_download = True
+            if to_download:
+                ret = urllib2.urlopen(url_href)
+                if ret.code == 200:
+                    print("{} {}".format(ret.code, url_href))
+                    os.system("wget {}".format(url_href))
+                    download.append(url_href)
+                else:
+                    print("Error :{}".format(ret.code))
+            if url_href.endswith("/"):
+                if not 'Parent Directory' in link.text:
+                    get_Href_From_Url(url_href)
         except :
             pass
     return download
@@ -41,12 +61,14 @@ def get_Href_From_Url(url):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="WGET Index of.")
     parser.add_argument('-url', help="Url ""index of"" site", default=None)
+    parser.add_argument('-ext', help="Extension to Download", default="")
+    parser.add_argument('-contains', help="contains any string in the text of href", default="")
     args = parser.parse_args()
     url = args.url
+    ext = args.ext
+    text = args.contains
     download_url = get_Href_From_Url(url)
     if download_url:
         print("{} file download!".format(len(download_url)))
     else:
         print("This URL {} not is a 'index of' page".format(url))
-
-
